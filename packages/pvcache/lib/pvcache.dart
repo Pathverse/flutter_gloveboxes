3a52054e769421195f49a484bd7a84484f9da161
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-09-09 13:49:34 -0700

    feat: add a toplv extendable component

--- packages/pvcache/lib/pvcache.dart
+++ packages/pvcache/lib/pvcache.dart
@@ -1,6 +1,12 @@
+import 'package:pvcache/src/top.dart';
+
 export 'src/adapter.dart';
 
 export 'src/cache.dart' show PVCache;
 
 export 'src/callframe.dart' show PVCFrame;
 export 'src/interface.dart' show PVBaseStorage, PVCtx;
+
+/// helper to extend on
+// ignore: non_constant_identifier_names
+final PVCACHE = TopLv();
