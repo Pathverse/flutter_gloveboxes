862ad8ce3ede3f14ef8b0c51ef86e00add106bea
ZackaryW <gitzackw@gmail.com>
2025-10-01 19:10:58 -0700

    ref(pvlogger): legacy zip for compl

--- legacy/pvlogger/lib/src/utils.dart
+++
@@ -1,8 +0,0 @@
-Map<String, dynamic> forbidPVLoggerPrefixInMetadata(Map<String, dynamic> metadata) {
-  for (var entry in metadata.entries) {
-    if (entry.key.startsWith("pvlogger_")) {
-      throw ArgumentError("PVLogger prefix is not allowed in metadata");
-    }
-  }
-  return metadata;
-}
